5648256ef681412f3d8f305d584cbc859405542f
“Dibyajyoti <dibya6148@gmail.com>
2025-06-22 02:02:19 +0530

    captain register

--- backend/models/captain.model.js
+++ backend/models/captain.model.js
@@ -12,56 +12,56 @@ const captainSchema = new mongoose.Schema({
         lastname : {
             type : String, 
             minlength : [3, 'lastname must be 3 characters long']
-        },
-        email : {
-            type: String,
-            required : true ,
-            unique : true ,
-            match : [/^\S+@\S+\.\S+$/ ,'Please enter a valid email']
-        },
-        password : {
+        }
+    },
+    email : {
+        type: String,
+        required : true ,
+        unique : true ,
+        match : [/^\S+@\S+\.\S+$/ ,'Please enter a valid email']
+    },
+    password : {
+        type : String,
+        required: true, 
+        select : false
+    },
+    socketId : {
+        type : String
+    },
+    status : {
+        type : String, 
+        enum : ['active', 'inactive'],
+        default : 'inactive'
+    },
+    vehicle : {
+        color : {
             type : String,
-            required: true, 
-            select : false
+            required : true,
+            minlength : [3, 'Enter valid color of vehicle ']
         },
-        socketId : {
-            type : String
+        plate : {
+            type : String,
+            required : true,
+            minlength : [3, 'Invalid plate number']
         },
-        status : {
-            type : String, 
-            enum : ['active', 'inactive'],
-            default : 'inactive'
+        capacity : {
+            type : Number,
+            required : true,
+            minlength : [1, ' Capacity must be more than 0']
         },
-        vehicle : {
-            color : {
-                type : String,
-                required : true,
-                minlength : [3, 'Enter valid color of vehicle ']
-            },
-            plate : {
-                type : String,
-                required : true,
-                minlength : [3, 'Invalid plate number']
-            },
-            capacity : {
-                type : Number,
-                required : true,
-                minlength : [1, ' Capacity must be more than 0']
-            },
-            vehicleType : {
-                type : String,
-                required : true ,
-                enum : ['car','motorcycle', 'auto']
-            }
-           
+        vehicleType : {
+            type : String,
+            required : true ,
+            enum : ['car','motorcycle', 'auto']
+        }
+        
+    },
+    location : {
+        lat : {
+            type : Number
         },
-        location : {
-            lat : {
-                type : Number
-            },
-            lng : {
-                type : Number
-            }
+        lng : {
+            type : Number
         }
     }
 }) 
